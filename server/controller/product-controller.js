313b1e7a10eff4e1146656013383b07e4810b102
Notnobcoder <andresolut@gmail.com>
2024-09-16 00:27:07 +0530

    rtest upload

--- server/controller/product-controller.js
+++ server/controller/product-controller.js
@@ -3,7 +3,11 @@ import Product from "../model/product-schema.js";
 export const getProducts = async (request, response) => {
   try {
     const products = await Product.find({});
-    response.status(200).json(products);
+
+    if (!products) {
+      return response.status(400).json({ message: "not working" })
+    }
+    return response.status(200).json(products);
   }
   catch (error) {
     response.status(500).json({ message: error.message });
